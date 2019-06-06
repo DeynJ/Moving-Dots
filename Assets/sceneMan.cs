172885b561560e1cbd9d8d992740a13d847806ef
DeynJ <51455408+DeynJ@users.noreply.github.com>
2019-06-06 15:59:01 -0400

    Asset Folder

---
+++ Assets/sceneMan.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneMan : MonoBehaviour {
+
+	// Use this for initialization
+	public void NewGameBtn(string newGameLevel){
+
+		SceneManager.LoadScene (newGameLevel);
+	}
+}
